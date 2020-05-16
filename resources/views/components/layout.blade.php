3f49d0d44a04bf4fb6bffa1d4165655b8f6e743c
Bruno Cesar <bruunoocesar@gmail.com>
2020-05-15 23:53:16 -0300

    Separa navbar em um componente

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="shortcut icon" href="/assets/images/favicon.ico">
+        @stack('components')
+        <link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="/assets/css/icons.min.css" rel="stylesheet" type="text/css"/>
+        <link href="/css/custom.css" rel="stylesheet" type="text/css"/>
+        @stack('css')
+        <title>@yield('titulo')</title>
+    </head>
+
+    <body>
+        @include('components.navbar')
+
+        <div class="container">
+            <div class="jumbotron">
+                <h1>@yield('cabecalho')</h1>
+            </div>
+
+            @yield('conteudo')
+        </div>
+
+        @stack('body')
+            <script src="/assets/js/vendor.min.js"></script>
+    </body>
+</html>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,64 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-primary fixed-top d-flex">
+    <div class="container d-flex">
+        <a class="navbar-brand" href="https://brnocesar.github.io/">brnocesar</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="container navbar-nav mr-auto">
+                <span class="navbar-cont">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ route('listar_series') }}">Séries</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Filmes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownTeste" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Teste
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownTeste">
+                            <a class="dropdown-item disabled" href="#">Teste 1</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item disabled" href="#">Teste 2</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item disabled" href="#">Teste 3</a>
+                        </div>
+                    </li>
+                </span>
+
+                <span class="navbar-user">
+                    @auth
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownUser" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <strong>{{ Auth::user()->name }}</strong>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownUser">
+                            <a class="dropdown-item disabled" href="#">Conta</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="{{-- dropdown-item --}} navi-link btn btn-danger" href="{{ route('deslogar') }}">
+                                <i class="fas fa-sign-out-alt mr-2"></i>Sair
+                            </a>
+                        </div>
+                    </li>
+                    @endauth
+                    
+                    @guest
+                    <li class="nav-item dropdown">
+                        <span>
+                            <a class="navi-link btn btn-light mb-2 mr-2" href="{{ route('pagina_registro') }}">
+                                <i class="fas fa-user-plus mr-2"></i>Criar usuário
+                            </a>
+                            <a class="navi-link btn btn-success mb-2 mr-2 form-auth" href="{{ route('pagina_login') }}">
+                                <i class="fas fa-sign-in-alt mr-2"></i>Entrar
+                            </a>
+                        </span>
+                    </li>
+                    @endguest
+                </span>
+            </ul>
+        </div>
+    </div>
+</nav>
