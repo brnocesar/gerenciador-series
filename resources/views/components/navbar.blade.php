51a6d8eef32c8a544202188716cd108f495cc996
Bruno Cesar <bruunoocesar@gmail.com>
2020-05-16 23:22:20 -0300

    Adiciona ajustes no estilo da navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,12 +1,12 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-primary fixed-top d-flex">
-    <div class="container d-flex">
+<nav class="navbar navbar-expand-lg navbar-light bg-primary fixed-top">
+    <div class="container">
         <a class="navbar-brand" href="https://brnocesar.github.io/">brnocesar</a>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="container collapse navbar-collapse align-content-center" id="navbarSupportedContent">
             <ul class="container navbar-nav mr-auto">
                 <span class="navbar-cont">
                     <li class="nav-item 
@@ -22,8 +22,8 @@
                     <li class="nav-item">
                         <a class="nav-link" href="#">Filmes</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownTeste" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownTeste" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-reference="parent">
                             Teste
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdownTeste">
@@ -54,11 +54,11 @@
                     
                     @guest
                     <li class="nav-item dropdown">
-                        <span>
-                            <a class="navi-link btn btn-light mb-2 mr-2 form-register" href="{{ route('pagina_registro') }}">
+                        <span class="navbar-guest">
+                            <a class="navi-link btn btn-light btn-sm mb-2 mr-2 form-register" href="{{ route('pagina_registro') }}">
                                 <i class="fas fa-user-plus mr-2"></i>Criar usuário
                             </a>
-                            <a class="navi-link btn btn-success mb-2 mr-2 form-auth" href="{{ route('pagina_login') }}">
+                            <a class="navi-link btn btn-success btn-sm mb-2 mr-2 form-auth" href="{{ route('pagina_login') }}">
                                 <i class="fas fa-sign-in-alt mr-2"></i>Entrar
                             </a>
                         </span>
