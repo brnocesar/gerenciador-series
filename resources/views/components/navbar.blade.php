1c6e68e4eb2a9b3ad1220ce7d94f0e6102a17e30
Bruno Cesar <bruunoocesar@gmail.com>
2020-06-19 20:23:54 -0300

    Limpa a pasta public e adiciona logo na nav

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="https://brnocesar.github.io/">brnocesar</a>
+        <a class="navbar-brand" href="https://brnocesar.github.io/">
+            <span class="row brand-profile-pic align-items-center">
+                <div class="brand-circle">
+                    <img src="{{asset('assets/images/laravel3.png')}}">
+                </div>
+                <span>@brnocesar</span>
+            </span>
+        </a>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
