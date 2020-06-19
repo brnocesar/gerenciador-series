c757a31641e1e3ca71869beee1de3561befc054b
Bruno Cesar <bruunoocesar@gmail.com>
2020-06-19 20:28:00 -0300

    Refatora trait de flash messages

--- app/Helpers/FlashMessages.php
+++ app/Helpers/FlashMessages.php
@@ -6,26 +6,19 @@ use Illuminate\Http\Request;
 
 trait FlashMessages
 {
-    protected $request;
-
-    public function __construct(Request $request)
-    {
-        $this->request = $request;
-    }
-
     public function flashMessage(array $messages)
     {
-        $this->request->session()->flash('flashMessage', $messages);
+        session()->flash('flashMessage', $messages);
     }
 
     public function getMessages()
     {
-        return $this->request->session()->get('flashMessage');
+        return session()->get('flashMessage');
     }
 
     public function notFound(string $model)
     {
-        $this->flashMessage([trans('messages.'. $model .'.not_found')]);
+        $this->flashMessage([trans("messages.$model.not_found")]);
         return redirect()->back();
     }
 }
